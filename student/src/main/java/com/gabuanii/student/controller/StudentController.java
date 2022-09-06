8f87d697e5f61793b0c2b92602b1e6e89167df5f
GilbertII <gilbert.abuan.ii@gmail.com>
2022-09-06 10:07:54 +0800

    udpate

--- student/src/main/java/com/gabuanii/student/controller/StudentController.java
+++ student/src/main/java/com/gabuanii/student/controller/StudentController.java
@@ -1,10 +1,11 @@
 package com.gabuanii.student.controller;
 
-import com.gabuanii.student.entity.Student;
 import com.gabuanii.student.request.CreateStudentRequest;
 import com.gabuanii.student.response.StudentResponse;
 import com.gabuanii.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/student")
+@RefreshScope
 public class StudentController {
 	
 	@Autowired
 	StudentService studentService;
+
+	@Value("${prop.type}")
+	private String type;
 	
 	@PostMapping("/create")
 	public StudentResponse createStudent (@RequestBody CreateStudentRequest studentRequest) {
@@ -27,6 +32,11 @@ public class StudentController {
 	@GetMapping("getById/{id}")
 	public StudentResponse getById (@PathVariable long id) {
 		return studentService.getById(id);
+	}
+
+	@GetMapping("/test")
+	public String test () {
+		return type;
 	}
 	
 }
--- student/src/main/java/com/gabuanii/student/feign/AddressFeignClient.java
+++ student/src/main/java/com/gabuanii/student/feign/AddressFeignClient.java
@@ -6,11 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-        name = "api-gateway/address-service"
-        // url = "${address.service.url}", // use this without eureka server
-        // path = "/api/address" // no longer used when api-gateway declare it in abstract method
-)
+@FeignClient(name = "api-gateway/address-service")
 public interface AddressFeignClient {
 
     @GetMapping("/api/address/getAddress/{id}")
